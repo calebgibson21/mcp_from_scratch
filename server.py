845a718f23474aa6d9cdec331484bba6c9ccea5a
Caleb <calebgibson21@gmail.com>
2025-06-08 12:46:47 -0500

    add required handlers

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from ast import Call
 import sys
 import json 
 import logging
@@ -47,3 +48,40 @@ class JsonRpcMessage:
     # check if this is a response (has id but no method)
     def is_response(self) -> bool:
         return self.method is None and self.id is not None
+
+class MCPServer:
+    """
+    Main MCP Server implementation with stdio transport.
+    """
+
+    def __init__(self):
+        """Initialize the server with empty handler registry."""
+        # resigrty of method handlers: method_name -> handler_function
+        self.handlers: Dict[str, Callable] = {}
+
+        # server state tracking
+        self.running = False
+        self.initialized = False
+
+        # register built in handlers
+        self._register_builtin_handlers()
+
+    def _register_builtin_handlers(self):
+        """Register essential MCP protocol handlers"""
+        self.handlers['initialize'] = self._handle_initialize
+        self.handlers['notifications/intitialized'] = self._handle_initialized
+        self.handlers['ping'] = self._handle_ping
+
+    def register_handler(self, method: str, handler: Callable):
+        """Register a handler function for a specified RPC method
+        
+        Args:
+            method: The RPC method name (e.g. 'tools/call')
+            handler: Functio that takes (params, message_id) and returns the result
+        """
+        # update the handlers registry
+        self.handlers[method] = handler
+
+    
+        
+
